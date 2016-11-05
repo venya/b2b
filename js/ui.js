63d019af128af228a198909b9e4c1eac2f456cba
Veniamin Zolotukhin <venyaz@gmail.com>
2016-11-05 08:00:20 +0200

    sample JSON files with data (to be loaded with JS)

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,18 @@
+
+function scrolledList(DOMselector, JSONpath) {
+	console.log('Attach to '+DOMselector);
+	console.log('TODO: load data from '+JSONpath);
+}
+
 $(function() {
+
 	// Init dropdowns
 	$('.dropdown__handle').click(function(e) {
 		e.preventDefault();
 		console.log('click');
 		$(this).parent('.dropdown').toggleClass('open');
 	});
+
+	var categories = new scrolledList($(".categories-list"), "data/categories.json");
+	var products = new scrolledList($(".products-list"), "data/products.json");
 });
