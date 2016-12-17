f0c44efba960bfd8c813e39f86bab155fb3de3fe
Veniamin Zolotukhin <venyaz@gmail.com>
2016-12-17 14:21:05 +0200

    Sample Durandal app with one view

---
+++ app/home.js
@@ -0,0 +1,13 @@
+define(function(require) {
+
+	var app = require('durandal/app');
+	var ko = require('knockout');
+
+	return {
+		name: ko.observable(),
+	// 	sayHello: function() {
+	// 		app.showMessage('Hello ' + this.name() + '! Nice to meet you.', 'Greetings');
+	// 	}
+	};
+
+});
---
+++ app/main.js
@@ -0,0 +1,28 @@
+requirejs.config({
+	paths: {
+		'durandal':	"../vendor/durandal/js",
+		'plugins':	"../vendor/durandal/js/plugins",
+		// 'transitions':"../vendor/durandal/js/transitions",
+		'knockout':	"../vendor/knockout/knockout-3.4.0",
+		'jquery':	"../vendor/jquery/jquery-1.9.1",
+		'text':		"../vendor/require/text",
+		}
+});
+
+define(function(require) {
+	var app = require('durandal/app');
+
+	var system = require('durandal/system');
+	system.debug(true);
+
+	app.title = 'Durandal Example';
+
+	app.configurePlugins({
+		// router:true,
+		// dialog: true
+	});
+
+	app.start().then(function() {
+		app.setRoot('home');
+	});
+});
