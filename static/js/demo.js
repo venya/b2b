eea1255558d4ece9de0ceeb7f350592c0a8032dc
Veniamin Zolotukhin <vzolotukhin@b2bsoft.com>
2017-02-02 14:06:53 +0200

    demo.js includes all quick JS handlers for reused effects. Headers added to UI-Kit page

---
+++ static/js/demo.js
@@ -0,0 +1,26 @@
+// This is collection of quick and dirty functions
+// to bring to life some GUI components, while no
+// JavaScript framework is defined in this project.
+
+
+
+function toggleClass(element, className, newState) {
+	if (newState == undefined) {
+		// determine current state, inverted
+		newState = !element.classList.contains(className);
+	}
+	//console.log(element, className, newState);
+	if (newState) {
+		element.classList.add(className);
+	} else {
+		element.classList.remove(className);
+		//element.className.replace(new RegExp('(?:^|\\s)'+ className + '(?:\\s|$)'), '');
+	}
+}
+
+
+function menuClose() {
+	toggleClass(menu, 'menu-active', false);
+	return false;
+}
+
