a08f0f9461094af5ee9c9475595bac8e7fe49820
Veniamin Zolotukhin <vzolotukhin@b2bsoft.com>
2017-02-07 17:00:12 +0200

    Stop compiling old sass sources

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
-	gulp.src('./sass/**/*.scss')
+	gulp.src('./sass/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./static/css'));
 });
